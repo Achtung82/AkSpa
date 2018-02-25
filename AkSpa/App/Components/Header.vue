b4fadd9829805240058d819bc75a32276cf0faf1
Achtung82 <l.hagmar@gmail.com>
2018-02-25 13:36:38 +0100

    Initalt för loginmodal

--- AkSpa/App/Components/Header.vue
+++ AkSpa/App/Components/Header.vue
@@ -1,31 +1,44 @@
 ﻿<template>
-<header>
-    <div class="container">
-        <router-link class="logo" to="/">
-            <img src="/images/logo.png" alt="Alte Kamereren &amp; Kamrérbaletten">
-        </router-link>
-    </div>
-    <nav>
-        <router-link v-if="menus"
-                     v-for="menu in menus"
-                     class="menu-link"
-                     v-bind:key="menu.url"
-                     :to="menu.url">
-            {{menu.text}}
-        </router-link>
-        <router-link v-if="menus"
-                     class="menu-link"
-                     v-bind:key="'/upcomming'"
-                     to="/upcomming">På gång
-        </router-link>
-    </nav>
-</header>
+    <header>
+        <div class="container">
+            <router-link class="logo" to="/">
+                <img src="/images/logo.png" alt="Alte Kamereren &amp; Kamrérbaletten">
+            </router-link>
+            <a class="account hidden-xs" @click="showModal = true">Logga in</a>
+        </div>
+        <nav>
+            <router-link v-if="menus"
+                         v-for="menu in menus"
+                         class="menu-link"
+                         v-bind:key="menu.url"
+                         :to="menu.url">
+                {{menu.text}}
+            </router-link>
+            <router-link v-if="menus"
+                         class="menu-link"
+                         v-bind:key="'/upcomming'"
+                         to="/upcomming">
+                På gång
+            </router-link>
+        </nav>
+        <login-modal :show="showModal" @close="showModal = false"></login-modal>
+    </header>
 </template>
 <script>
+    import LoginModal from './LoginModal.vue';
+
     export default {
+        components: {
+            LoginModal
+        },
         computed: {
             menus: function () {
                 return this.$store.getters.menus;
+            }
+        },
+        data: function () {
+            return {
+                showModal: false
             }
         }
     }
@@ -64,5 +77,13 @@
     }
     nav .menu-link.router-link-exact-active {
         color: $akmenulinkgrey;
+    }
+    .account {
+        color: $akblack;
+        position: absolute;
+        top: 0;
+        right: 15px;
+        cursor: pointer;
+        font-weight: 500;
     }
 </style>

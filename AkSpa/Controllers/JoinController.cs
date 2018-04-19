d553550231afe1682491b1a37a9cb15bbc3fce54
Achtung82 <l.hagmar@gmail.com>
2018-04-19 20:47:42 +0200

    Adding controller for join form

---
+++ AkSpa/Controllers/JoinController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AkSpa.Models;
+using AKSpa.DataModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkSpa.Controllers
+{
+    [Route("api/[controller]")]
+    public class JoinController : Controller
+    {
+        private readonly AkContext _db;
+        public JoinController(AkContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        public JsonResult Join(JoinUsModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Email) && string.IsNullOrWhiteSpace(model.Tel))
+                return Json(new { success = false, message = "Du har ej angett ett sätt att kontakta dig med." });
+            if (string.IsNullOrWhiteSpace(model.Instrument))
+                return
+                    Json(
+                        new
+                        {
+                            success = false,
+                            message =
+                                "Du måste ange vilke instrument du spelar eller om du vill dansa med baletten."
+                        });
+
+            if (
+                _db.Recruits.Any(
+                    x =>
+                        (!string.IsNullOrWhiteSpace(model.Email) && (x.Email == model.Email)) ||
+                        (!string.IsNullOrWhiteSpace(model.Tel) && (x.Phone == model.Tel))))
+                return
+                    Json(
+                        new
+                        {
+                            success = false,
+                            message = "En person med din kontaktinformation har redan anmält sig."
+                        });
+            var rec = new Recruit
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Created = DateTime.UtcNow,
+                Email = model.Email,
+                Phone = model.Tel,
+                Instrument = model.Instrument,
+                Other = model.Other
+            };
+            _db.Recruits.Add(rec);
+            _db.SaveChanges();
+
+            return
+                Json(new { success = true, message = "Din ansökan är mottagen och vi kommer kontakta dig inom kort" });
+        }
+    }
+}
---
+++ AkSpa/Models/JoinUsModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AkSpa.Models
+{
+    public class JoinUsModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Instrument { get; set; }
+        public string Other { get; set; }
+    }
+}

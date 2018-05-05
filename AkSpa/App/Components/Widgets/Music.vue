8da0d16a722443d2f078bf23e3d55a2ae1b5e845
Achtung82 <l.hagmar@gmail.com>
2018-05-05 17:32:14 +0200

    Listing albums

--- AkSpa/App/Components/Widgets/Music.vue
+++ AkSpa/App/Components/Widgets/Music.vue
@@ -1,6 +1,13 @@
 ﻿<template>
     <div class="music">
-        <span v-for="album in albums">{{album}}</span>
+        <div class="album-list">
+            <div class="album" v-for="album in albums">
+                <a href="#" class="album-link" v-on:click.prevent="albumClick(album)">
+                    <img class="album-img" :src="getImgLink(album)" />
+                    <p class="name">{{album.name}}</p>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,10 +20,40 @@
             albums: function () {
                 return this.$store.getters.albums;
             }
+        },
+        methods: {
+            getImgLink: function(album) {
+                return "https://www.altekamereren.org/" + album.image;
+            },
+            albumClick: function (album) {
+                console.log(album.name);
+            }
         }
     }
 </script>
 <style lang="scss">
     @import "../../Styles/variables.scss";
-    
+    .album-list {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .album-img {
+        height: 100px;
+        max-width: 100px;
+    }
+    .album {
+        padding: 10px 10px;
+        width: 130px;
+        position: relative;
+    }
+    .album-link {
+        width: 100%;
+        display: block;
+        text-align: center;
+    }
+    .album .name {
+        text-align: center;
+        font-size: 0.9rem;
+        margin-top: 0;
+    }
 </style>
